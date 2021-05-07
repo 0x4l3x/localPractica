68d86ea40a231c4f66249a5b4256174b24a7f178
alejandro-bmorante-alumnos-upm-es <alejandro.bmorante@alumnos.upm.es>
2021-05-07 21:22:18 +0200

    Wtf pasa en el main

--- src/main/java/etsisi/ems2020/trabajo3/lineadehorizonte/Main.java
+++ src/main/java/etsisi/ems2020/trabajo3/lineadehorizonte/Main.java
@@ -13,10 +13,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		LineaHorizonte l1=CalcularLineaHorizonteCiudad();
-		if(resultadoCorrecto(l1))
 			System.out.println("-- Proceso finalizado Correctamente --");
-		else
-			System.out.println("-- Proceso finalizado incorrectamente --");
+
 
 
 		//eliminadas dos lineas de codigo useless
@@ -28,20 +26,5 @@ public class Main {
 		linea = c.getLineaHorizonte();
 		linea.guardaLineaHorizonte("salida.txt");
 		return linea;
-	}
-	public static boolean resultadoCorrecto(LineaHorizonte linea) {//Funciona mal y no se si hay que arreglarlo
-		if(
-				(linea.getPunto(0).getX()== 1  && linea.getPunto(0).getY()==4)||
-				(linea.getPunto(1).getX()== 2  && linea.getPunto(1).getY()==7)||
-				(linea.getPunto(2).getX()== 6  && linea.getPunto(2).getY()==9)||
-				(linea.getPunto(3).getX()== 8  && linea.getPunto(3).getY()==7)||  
-				(linea.getPunto(4).getX()== 9  && linea.getPunto(4).getY()==4)||
-				(linea.getPunto(5).getX()== 11 && linea.getPunto(5).getY()==6)||
-				(linea.getPunto(6).getX()== 13 && linea.getPunto(6).getY()==0)||
-				(linea.getPunto(7).getX()== 14  && linea.getPunto(7).getY()==2)||
-				(linea.getPunto(8).getX()== 15  && linea.getPunto(8).getY()==0))
-			return true;
-		else 
-			return false;
 	}
 }
